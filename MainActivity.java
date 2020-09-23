3dc0057c1f28a4ed1fd13b313ef5943207b2b4d3
HuZhiKui <71072639+HuZhiKui@users.noreply.github.com>
2020-09-23 09:58:46 +0800

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -6,8 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -18,15 +22,58 @@ public class MainActivity extends AppCompatActivity {
     Button btn1,btn2;
     ProgressDialog mydialog;
     LinearLayout login;
+    private static String TAG = "LIFTCYCLE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "(1) onCreate()");
 
         btn1=(Button)findViewById(R.id.bu1);
         btn1.setOnClickListener(new mClick());
+        Button button=(Button)findViewById(R.id.bu);
+        button.setOnClickListener(new View.OnClickListener() {public void onClick(View v) {
+        Intent intent=new Intent(MainActivity.this,AndroidActivity.class);
+        intent.putExtra("name","Zhangsan");
+        intent.putExtra("age",20);
+        startActivity(intent);
+
     }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "(3) onResume()");
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "(2) onStart()");
+    }
+
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "(5) onRestart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "(4) onStop()");
+    }
+
 
     class mClick implements View.OnClickListener {
         AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
